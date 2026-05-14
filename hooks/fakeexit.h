58b83e59a7738af70564709646ea1772e4e00f1e
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-05-14 19:39:33 +0200

    alt f4 conditional autosave

--- hooks/fakeexit.h
+++ hooks/fakeexit.h
@@ -1,11 +1,22 @@
 int FakeExitFlag = 0;
 
+// only do alt f4 autosave if we're not in the infractions screen
+// (infractions due are not saved in the savegame)
+bool DoAltF4Autosave() {
+	if (TheGameFlowManager.CurrentGameFlowState != GAMEFLOW_STATE_IN_FRONTEND) return false;
+
+	if (cFEng::mInstance->IsPackagePushed("Dialog.fng")) return false;
+	if (cFEng::mInstance->IsPackagePushed("Infractions.fng")) return false;
+	return true;
+}
+
 bool CanAltF4() {
 	// allow alt f4 if autosave is off
 	//if (!GetUserProfile()->TheOptionsSettings.TheGameplaySettings.AutoSaveOn) return false;
 
 	// in-game, only block alt f4 if in a pursuit or nis (general nis check but should be for busted nis)
 	if (TheGameFlowManager.CurrentGameFlowState == GAMEFLOW_STATE_RACING) {
+		if (IsInLoadingScreen()) return false;
 		if (IsInNIS()) return false;
 		auto ply = GetLocalPlayerInterface<IPerpetrator>();
 		if (ply && ply->IsBeingPursued()) return false;
@@ -15,9 +26,11 @@ bool CanAltF4() {
 	// otherwise make sure a savegame has been loaded and we're not in the main menu
 	if (IsInSplashScreenOrIntros()) return true;
 
-	if (FEDatabase->FEGameMode == eFE_GAME_MODE_NONE) return true;
-	if (FEDatabase->FEGameMode == eFE_GAME_MODE_BEAT_GAME) return true;
-	if (FEDatabase->FEGameMode == eFE_GAME_MODE_POST_RIVAL) return true;
+	if (TheGameFlowManager.CurrentGameFlowState == GAMEFLOW_STATE_IN_FRONTEND) {
+		if (cFEng::mInstance->IsPackagePushed("MainMenu.fng")) return true;
+		if (FEDatabase->FEGameMode == eFE_GAME_MODE_BEAT_GAME) return true;
+		if (FEDatabase->FEGameMode == eFE_GAME_MODE_POST_RIVAL) return true;
+	}
 
 	return false;
 }
@@ -36,11 +49,13 @@ void FakeExitLoop() {
 			ExitTheGameFlag = FakeExitFlag;
 		}
 		else {
-			// this is funny but autosave doesnt save infractions due
-			//EAutoSave::Create();
-			//CwoeeHints::AddHint("Game Saved.");
-
-			CwoeeHints::AddHint("No :3");
+			if (DoAltF4Autosave()) {
+				EAutoSave::Create();
+				CwoeeHints::AddHint("Game Saved.");
+			}
+			else {
+				CwoeeHints::AddHint("No :3");
+			}
 		}
 		FakeExitFlag = 0;
 	}
